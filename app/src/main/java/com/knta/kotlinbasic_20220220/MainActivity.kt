e69a966a383aedb56d5580cc5efa6338bf7884d1
sk.you1226 <you1226@naver.com>
2022-02-20 14:34:16 +0900

    버튼 활성화

--- app/src/main/java/com/knta/kotlinbasic_20220220/MainActivity.kt
+++ app/src/main/java/com/knta/kotlinbasic_20220220/MainActivity.kt
@@ -7,26 +7,53 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        Ctrl + / => 현재 줄을 주석으로.
 
-        // 아무말 => 메모 남김
-        // 단축키는 Ctrl + /
-
-//        btnLog 버튼이 눌리면 해야할 일
+//        btnLog 버튼이 눌리면 해야할 일을 지정.
         btnLog.setOnClickListener {
 
-//            로그 버튼이 눌릴때마다 실행될 코드
-//            로그를 찍어 보자  이벤트 처리
+//            로그 버튼이 눌릴때마다 실행될 코드 {  }
 
-            Log.d("메인화면", "로그 버튼 눌림")
+//            로그를 찍어보자.
+            Log.d( "메인화면", "로그 버튼 눌림" )
             Log.e("메인화면", "e로 로그 찍어보기")
 
         }
 
+        btnToast.setOnClickListener {
 
+//            토스트를 띄워보자.
+            Toast.makeText(this, "토스트 버튼 눌림", Toast.LENGTH_SHORT).show()
+
+
+        }
+
+        btnVariablePractice.setOnClickListener {
+
+//            val / var 에 대한 연습/체험
+
+            var myName : String // 내용 변경 가능
+
+            val friendName : String // 한번 대입한 값으로 고정
+
+//            myName - 내 이름, friendName - 친구 이름
+
+            myName = "조경진"
+
+            friendName = "조민상"
+
+//            내이름 / 친구이름 다시 대입 (데이터 변경)
+
+            myName = "조진경" // 추가로 다시 대입해도 됨. var
+//            friendName = "조상민" // 한번 넣은걸로 계속 사용. val
+
+
+        }
 
     }
 }
